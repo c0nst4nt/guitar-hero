51a7a677e17f000532555d0db170225da371b051
Constantin Gordienko <gordienko.c@gmail.com>
2021-01-19 00:37:37 +0200

    Small naming fix.

--- bin/testCarplusStrong.php
+++ bin/testCarplusStrong.php
@@ -6,8 +6,8 @@ require_once './vendor/autoload.php';
 
 ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 
-// A note (1 octave)
+// A note (4 octave)
 $aNoteFrequency = 440.0;
 $synth = new GuitarStringSynthesizer($aNoteFrequency, 1);
-$synth->writeToFile(__DIR__  . '/../generated/audio/a_1.wav');
+$synth->writeToFile(__DIR__  . '/../generated/audio/a_4.wav');
 $synth->drawWavePicture('String sound wave (Carplus-Strong)', __DIR__  . '/../generated/images/wave-carplus-strong.png');
